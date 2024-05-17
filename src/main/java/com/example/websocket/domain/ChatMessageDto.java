c7f619836b5dc73b17f716adfd94be22086f508e
seonoo <tjsdn22@naver.com>
2024-05-17 16:44:03 +0900

    feat: Create Socket Communication for handler and dto

--- src/main/java/com/example/websocket/domain/ChatMessageDto.java
+++ src/main/java/com/example/websocket/domain/ChatMessageDto.java
@@ -1,2 +1,23 @@
-package com.example.websocket.domain;public class ChatMessageDto {
+package com.example.websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessageDto {
+
+    public enum MessageType{
+        ENTER, TALK
+    }
+
+    private MessageType messageType; // 메시지 타입
+    private Long chatRoomId; // 방 번호
+    private Long senderId; // 채팅을 보낸 사람 아이디
+    private String message; // 메시지
+
 }
