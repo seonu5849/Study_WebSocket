0ea723b490f6cf8e43852e4b99f61b0b3e0a2f8d
seonoo <tjsdn22@naver.com>
2024-05-20 17:37:34 +0900

    feat: #2 created login and main page of static design

---
+++ src/main/java/com/example/websocket/controller/ViewerController.java
@@ -0,0 +1,23 @@
+package com.example.websocket.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ViewerController {
+
+    @GetMapping("/login")
+    public String loginView() {
+        return "login";
+    }
+
+    @GetMapping("/main")
+    public String mainView() {
+        return "main";
+    }
+
+}
