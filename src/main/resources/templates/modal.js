cd50edb46d6ff473ed44d15b93b94e7e26ea17fb
seonoo <tjsdn22@naver.com>
2024-05-21 18:41:53 +0900

    feat: #2 created modal and modified other css

---
+++ src/main/resources/templates/modal.js
@@ -0,0 +1,15 @@
+
+const modal = document.querySelector('.modal');
+const modalOpen = document.querySelector('.modal-btn');
+const modalClose = document.querySelector('.close-btn');
+
+// button 태그 사용시 type="button" 을 해주지 않으면 js에서 인식을 못한다.
+
+// 열기 버튼을 눌렀을 때 모달팝업이 열림
+modalOpen.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+// 닫기 버튼을 눌렀을 때 모달팝업이 닫힘
+modalClose.addEventListener('click', function(){
+    modal.style.display = 'none';
+});
